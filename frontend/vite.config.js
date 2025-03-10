7f9d7caa7e2d87bc0c35b591a447a190f0c7af44
udayyyy09 <udaychaudhary419@gmail.com>
2025-03-10 22:54:55 +0530

    updated_vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: ".", // Explicitly setting the root
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure the output is in 'dist'
+  },
 })
