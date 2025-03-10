5be9fd10a67cc5772a2c1b00bfe2345d6f162fc1
udayyyy09 <udaychaudhary419@gmail.com>
2025-03-10 23:00:13 +0530

    index_change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  root: ".", // Explicitly setting the root
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure the output is in 'dist'
-  },
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react' // or whatever framework you're using
+
+// export default defineConfig({
+//   plugins: [react()],
+//   root: './', // Ensure this points to the directory containing your index.html
+// })
