7d2db8030d17d8ffae80d2d575c67dc21223c58b
udayyyy09 <udaychaudhary419@gmail.com>
2025-03-11 17:37:51 +0530

    vite_changes_again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
 })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react' // or whatever framework you're using
-
-// export default defineConfig({
-//   plugins: [react()],
-//   root: './', // Ensure this points to the directory containing your index.html
-// })
